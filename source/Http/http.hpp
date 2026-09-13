4a7425cefd9033b31a9440109516388975fc3aba
fantasy55 <2392033510@qq.com>
2026-09-13 23:21:59 +0800

    Url编码解码及一些配置模块

--- source/Http/http.hpp
+++ source/Http/http.hpp
@@ -185,14 +185,14 @@ public:
             return false;
         }
 
-        size_t fsize;          // 文件大小
+        size_t fsize = 0;      // 文件大小
         ifs.seekg(0, ifs.end); // 将光标移动到文件末尾
         fsize = ifs.tellg();   // end到文件起始处正好是文件大小
         ifs.seekg(0, ifs.beg); // 重新移动光标到开头处
 
         // 扩容
         buf->resize(fsize);
-        ifs.read(&(*buf)[0], fsize);
+        ifs.read(buf->data(), fsize); // c_str()返回的是const char*，data()返回的是char *,即可以被修改
 
         if (ifs.good() == false)
         {
@@ -202,5 +202,133 @@ public:
         }
         ifs.close();
         return true;
+    }
+
+    // 文件写入操作
+    static bool WriteFile(const std::string &filename, const std::string &buf)
+    {
+        std::ofstream ofs(filename, std::ios::binary | std::ios::trunc); // trunc截断清空数据
+        if (ofs.is_open() == false)
+        {
+            LOG(LOGLEVEL::ERR, "Open File:%s Failed!!!", filename.c_str());
+            return false;
+        }
+
+        ofs.write(buf.c_str(), buf.size()); // c_str()返回的是const char*，data()返回的是char *,即可以被修改
+
+        if (ofs.good() == false)
+        {
+            LOG(LOGLEVEL::ERR, "Write File:%s Failed!!!", filename.c_str());
+            ofs.close();
+            return false;
+        }
+        ofs.close();
+        return true;
+    }
+
+    // 编码格式：将特殊字符的ascii值，转换为两个16进制字符，前缀%   C++ -> C%2B%2B
+    // 不编码的特殊字符： RFC3986文档规定 . - _ ~ 字母，数字属于绝对不编码字符
+    // RFC3986文档规定，编码格式 %HH
+    // W3C标准中规定，查询字符串中的空格，需要编码为+， 解码则是+转空格
+    // url编码操作
+    static std::string UrlEncode(const std::string url, bool convert_space_to_plus)
+    {
+        std::string res;
+        for (auto &c : url)
+        {
+            if (c == '.' || c == '-' || c == '_' || c == '~' || isalnum(c)) // 特殊字符不编码
+            {
+                res += c;
+                continue;
+            }
+            if (c == ' ' && convert_space_to_plus == true) // 有开空格转加号则按W3C标准来
+            {
+                res += '+';
+                continue;
+            }
+            char tmp[4] = {0}; // 其余字符均需要编码,格式为%HH,%%输出为%
+            snprintf(tmp, 4, "%%%02X", c);
+            res += tmp;
+        }
+        return res;
+    }
+
+    // 十六进制转整数操作
+    static char HEXTOI(char c)
+    {
+        if (c >= '0' && c <= '9')
+        {
+            return c - '0';
+        }
+        else if (c >= 'a' && c <= 'f') // 十六进制a表示10
+        {
+            return c - 'a' + 10;
+        }
+        else if (c >= 'A' && c <= 'F')
+        {
+            return c - 'A' + 10;
+        }
+        else
+        {
+            return -1; // 非十六进制数字
+        }
+    }
+
+    // url解码操作
+    static std::string UrlDecode(const std::string url, bool convert_plus_to_space)
+    {
+        std::string res;
+        for (int i = 0; i < url.size(); i++)
+        {
+            const char c = url[i];
+            if (c == '+' && convert_plus_to_space == true) // 有开加号转空格则按W3C标准来
+            {
+                res += ' ';
+                continue;
+            }
+            if (c == '%' && (i + 2) < url.size()) // 其余字符均需要解码,编码格式为%HH
+            {
+                char v1 = HEXTOI(url[i + 1]);
+                char v2 = HEXTOI(url[i + 2]);
+                char v = v1 * 16 + v2; // 算回原来的数字
+                res += v;
+                i += 2; // 跳转到下一个需要解码的字符
+                continue;
+            }
+            res += c;
+        }
+        return res;
+    }
+
+    // 响应状态码的描述信息获取操作
+    static std::string StatuDesc(int statu)
+    {
+        auto it = _statu_msg.find(statu);
+        if (it == _statu_msg.end())
+        {
+            return "Unknown";
+        }
+        else
+        {
+            return it->second;
+        }
+    }
+
+    // 根据文件后缀名获取文件mime
+    static std::string ExtMime(const std::string &filename)
+    {
+        size_t pos = filename.find_last_of('.');
+        if (pos == std::string::npos)
+        {
+            return "application/octet-stream"; // 默认二进制流
+        }
+
+        std::string ext = filename.substr(pos);
+        auto it = _mime_msg.find(ext);
+        if (it == _mime_msg.end())
+        {
+            return "application/octet-stream"; // 默认二进制流
+        }
+        return it->second;
     }
 };
